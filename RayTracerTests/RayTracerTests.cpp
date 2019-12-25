76c6d82547020d31fa985cc566e33fe65599f549
Garrett Gunnell <gunnellg@oregonstate.edu>
2019-12-25 02:18:06 -0800

    Initial commit

---
+++ RayTracerTests/RayTracerTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RayTracerTests
+{
+	TEST_CLASS(RayTracerTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
