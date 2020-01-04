beb088d20ff26d55d5ad4fdaf06e9c3576e22e1e
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-01-03 18:36:37 -0800

    Fix numerous issues with color implementation, don't code at 5am

--- src/Color.cpp
+++ src/Color.cpp
@@ -1,7 +1,7 @@
 #include "Color.h"
 #include "Tuple.h"
 
-Color::Color(float r, float b, float g) {
+Color::Color(float r, float g, float b) {
 	this->red = r;
 	this->blue = b;
 	this->green = g;
@@ -13,17 +13,17 @@ bool operator== (const Color& c1, const Color& c2) {
 }
 
 Color operator+ (const Color& c1, const Color& c2) {
-	return Color(c1.red + c2.red, c1.blue + c2.blue, c1.green + c2.green);
+	return Color(c1.red + c2.red, c1.green + c2.green, c1.blue + c2.blue);
 }
 
 Color operator- (const Color& c1, const Color& c2) {
-	return Color(c1.red - c2.red, c1.blue - c2.blue, c1.green - c2.green);
+	return Color(c1.red - c2.red, c1.green - c2.green, c1.blue - c2.blue);
 }
 
 Color operator* (const Color& c, const float scalar) {
-	return Color(c.red * scalar, c.blue * scalar, c.green * scalar);
+	return Color(c.red * scalar, c.green * scalar, c.blue * scalar);
 }
 
 Color operator* (const Color& c1, const Color& c2) {
-	return Color(c1.red * c2.red, c1.blue * c2.blue, c1.green * c2.green);
+	return Color(c1.red * c2.red, c1.green * c2.green, c1.blue * c2.blue);
 }
